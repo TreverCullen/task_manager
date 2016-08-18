e968e2bccf68a1e911a1b6dd76fb7717f7bb8526
Trever Cullen <trevercullen@gmail.com>
2016-08-18 14:37:48 -0700

    init commit

---
+++ routes/base.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+router.get('/', function(req, res) {
+	res.render('home');
+});
+
+module.exports = router;
