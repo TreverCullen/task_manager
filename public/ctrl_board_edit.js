27d6074027e4cca8b84a9f4a84d25c18bf0f1a0b
Trever Cullen <trevercullen@gmail.com>
2016-11-15 16:04:04 -0500

    open board menu on edit cancel

--- public/ctrl_board_edit.js
+++ public/ctrl_board_edit.js
@@ -15,20 +15,18 @@ function($scope, $mdDialog, $mdToast){
 					title: $scope.title,
 					desc: $scope.desc
 				});
-				$mdDialog.show({
-					contentElement: '#list_board',
-					parent: angular.element(document.body),
-					targetEvent: event,
-					clickOutsideToClose: true
-				});
+				$scope.cancel();
 			}
 		}
 	};
 	// cancel task and clear form
 	$scope.cancel = function(){
-		$mdDialog.hide();
-		$scope.title = null;
-		$scope.desc = null;
+		$mdDialog.show({
+			contentElement: '#list_board',
+			parent: angular.element(document.body),
+			targetEvent: event,
+			clickOutsideToClose: true
+		});
 	};
 
 	$scope.DeleteBoard = function(){
